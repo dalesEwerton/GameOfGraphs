536aeff9e394256d8a22af79494e03b9aadd042b
dales.fragoso@embedded.ufcg.edu.br <dales.fragoso@embedded.ufcg.edu.br>
2018-11-07 13:01:52 -0300

    Setiing up the graph

---
+++ src/Actions.java
@@ -0,0 +1,33 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+
+public class Actions extends JFrame {
+	
+	private mxGraph graph;
+	private mxGraphComponent graphComponent;
+	
+	public Actions() {
+		super("JGraph - Flow");
+		initGUI();
+	}
+	
+	private void initGUI() {
+		setSize(800,600);
+		setLocationRelativeTo(null);
+		graph = new mxGraph();
+		graphComponent = new mxGraphComponent(this.graph);
+		graphComponent.setPreferredSize(new Dimension(400, 400));
+		getContentPane().add(graphComponent);
+		
+		Object parent = graph.getDefaultParent();
+		graph.getModel().beginUpdate();
+		graph.insertVertex(parent, null, "TEST", 30, 80, 100, 50);
+		
+		graph.getModel().endUpdate();
+	}
+
+}
