f1f4b187240c5675cd485c0f6f3e27321251fb97
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2018-11-08 23:09:20 -0200

    Add Mothers

--- src/Graph/Actions.java
+++ src/Graph/Actions.java
@@ -1,7 +1,14 @@
 package Graph;
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 import javax.swing.JFrame;
+
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DirectedAcyclicGraph;
 
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.view.mxGraph;
@@ -11,20 +18,30 @@ import Characters.Player;
 
 public class Actions extends JFrame {
 	
+	private DefaultDirectedGraph<String, DefaultEdge> gotGraph;
 	private mxGraph graph;
 	private mxGraphComponent graphComponent;
+	private HashMap<Object, Object> graphMap;
+	private HashMap<String, Object> gotMap;
+	private ArrayList<String> edgeSrc;
+	private ArrayList<String> edgeFont;
 	private int cordX = 30;
 	private int cordY = 30;
 	
 	public Actions() {
-		super("JGraph - Flow");
+		super("Game of Graphs - JGraph");
 		initGUI();
 	}
 	
 	private void initGUI() {
 		setSize(1280,768);
 		setLocationRelativeTo(null);
-		graph = new mxGraph();
+		graph    = new mxGraph();
+		gotGraph = new DefaultDirectedGraph<>(DefaultEdge.class);
+		graphMap = new HashMap<>();
+		gotMap   = new HashMap<>();
+		edgeSrc  = new ArrayList<>();
+		edgeFont = new ArrayList<>();
 		graphComponent = new mxGraphComponent(this.graph);
 		graphComponent.setPreferredSize(new Dimension(1280, 768));
 		getContentPane().add(graphComponent);
@@ -32,23 +49,61 @@ public class Actions extends JFrame {
 		Object parent = graph.getDefaultParent();
 		graph.getModel().beginUpdate();
 		 
-		this.loadCharsNodes();
+		this.loadGraphObject();
+		this.loadGraficGraph();
 		
 		graph.getModel().endUpdate();
 	}
 	
-	private void loadCharsNodes(){
+	private void loadGraphObject(){
 		CharsLoadder chrs = new CharsLoadder();
 		chrs.loadChars();
 		Player players[] = chrs.getChars();
-		Object parent = graph.getDefaultParent();
 		
 		for (Player c : players) {
-			this.graph.insertVertex(parent, c.getId() , c.getName(), 
-					this.cordX, this.cordY, 15, 15);
+			Object v = this.gotGraph.addVertex(c.getName());
+			this.gotMap.put(c.getName(), v);
+		}
+		
+		for (Player c : players) {
+			if (!c.getMotherName().equals("N/A")) {
+				try {
+					this.gotGraph.addEdge(c.getMotherName(), c.getName());
+					this.edgeSrc.add(c.getMotherName());
+					this.edgeFont.add(c.getName());
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+			} else if (!c.getFatherName().equals("N/A")) {
+				try {
+					this.gotGraph.addEdge(c.getName(), c.getFatherName());
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+	}
+	
+	private void loadGraficGraph() {
+		Set<String> plays = this.gotMap.keySet();
+		Object parent     = this.graph.getDefaultParent();
+		
+		for (String p : plays) {
+			Object v = this.graph.insertVertex(parent, null, p,
+					this.cordX, this.cordY, 20, 20);
+			this.graphMap.put(p, v);
 			this.updateCords();
 		}
 		
+		for (int i = 0 ; i < this.edgeSrc.size(); i++) {
+			try {
+				this.graph.insertEdge(parent, null, null,
+						this.graphMap.get(this.edgeSrc.get(i)),
+						this.graphMap.get(this.edgeFont.get(i)));
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
 	}
 	
 	private void updateCords() {
